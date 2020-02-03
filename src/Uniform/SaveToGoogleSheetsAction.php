65b2f686255ce325c679b6c3e0230283493f0da2
Cees van Egmond <cees.vanegmond@wearejust.com>
2020-02-03 16:11:37 +0100

    Update

--- src/Uniform/SaveToGoogleSheetsAction.php
+++ src/Uniform/SaveToGoogleSheetsAction.php
@@ -3,19 +3,40 @@
 namespace Wearejust\KirbySheets\Uniform;
 
 use Uniform\Actions\Action;
+use Uniform\Form;
 use Wearejust\KirbySheets\Connector;
 
 class SaveToGoogleSheetsAction extends Action
 {
+    /**
+     * @var \Wearejust\KirbySheets\Connector
+     */
+    private $connector;
+
+    public function __construct(Form $form, array $options = [])
+    {
+        $this->connector = new Connector();
+
+        parent::__construct($form, $options);
+    }
+
     public function perform()
     {
         $spreadsheetId = $this->option('spreadsheetId');
         $sheetName = $this->option('name');
 
-        $connector = new Connector();
-
-        if (! $connector->append($spreadsheetId, $sheetName, $this->form->data())) {
+        if (! $this->connector->append($spreadsheetId, $sheetName, $this->getData())) {
             $this->fail();
         }
+    }
+
+    private function getData() : array
+    {
+        $exclude = $this->option('exclude', []);
+        $exclude[] = '_form';
+
+        return array_filter($this->form->data(), function($key) use ($exclude) {
+            return ! in_array($key, $exclude, true);
+        }, ARRAY_FILTER_USE_KEY);
     }
 }
